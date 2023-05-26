ef15aa54f3b66a4fb5d18104a65b6889cece8359
AlexandreFelippe <alemfelippe@gmail.com>
2023-05-26 18:29:06 -0300

    mais uma tentativa no 03, o código JS não lê as variáveis

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 //Ao preencher o formulário e clicar no botão, será validado que:
 //Caso o email seja "tryber@teste.com" e a senha seja "123456", será emitido um alerta contendo o texto "Olá, Tryber!";
 //Em todos os outro casos deverá ser emitido um alerta contendo o texto "Email ou senha inválidos.";
-function isNameValid() {
-    const email = document.querySelector('#input-email');
-    const password = document.querySelector('input-password')
+const email = document.querySelector('#input-email');
+const password = document.querySelector('#input-password')
+const btn = document.getElementById('btn');
+btn.addEventListener('click', () => {
     if (email === "tryber@teste.com" & password === "123456") {
         return "Olá, Tryber!"
     }
     else{
         return "Email ou senha inválidos."
     }
-isNameValid();
-button.addEventListener('click', isNameValid());
+})
