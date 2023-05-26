6012254665e187efe07bc5b6e20c3a490d76c6ff
Thiago Pedrosa Woznika <thiagopedrosaw@gmail.com>
2023-05-26 18:47:46 -0300

    resolve 5 e 6 certo agr

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-//Ao preencher o formulário e clicar no botão, será validado que:
-//Caso o email seja "tryber@teste.com" e a senha seja "123456", será emitido um alerta contendo o texto "Olá, Tryber!";
-//Em todos os outro casos deverá ser emitido um alerta contendo o texto "Email ou senha inválidos.";
-const email = document.querySelector('#input-email');
-const password = document.querySelector('#input-password')
-const btn = document.getElementById('btn');
-btn.addEventListener('click', () => {
-    if (email === "tryber@teste.com" & password === "123456") {
-        return "Olá, Tryber!"
-    }
-    else{
-        return "Email ou senha inválidos."
-    }
-})
+// Ao preencher o formulário e clicar no botão, será validado que:
+// Caso o email seja "tryber@teste.com" e a senha seja "123456", será emitido um alerta contendo o texto "Olá, Tryber!";
+// Em todos os outro casos deverá ser emitido um alerta contendo o texto "Email ou senha inválidos.";
+
+// const email = document.querySelector('#input-email');
+// const password = document.querySelector('#input-password')
+// const btn = document.getElementById('btn');
+// btn.addEventListener('click', () => {
+//     if (email === "tryber@teste.com" & password === "123456") {
+//         return "Olá, Tryber!"
+//     }
+//     else{
+//         return "Email ou senha inválidos."
+//     }
+// })
+
+const main = document.getElementById('main');
+const form = document.createElement('form');
+main.appendChild(form);
+form.id = 'evaluation-form';
