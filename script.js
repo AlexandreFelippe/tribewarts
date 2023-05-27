b21183a20645bfbcb68eac0e98581c1e13fb1f8b
Thiago Pedrosa Woznika <thiagopedrosaw@gmail.com>
2023-05-27 00:22:02 -0300

    Corrige form para funcionar

--- script.js
+++ script.js
@@ -13,35 +13,3 @@
 //         return "Email ou senha inválidos."
 //     }
 // })
-
-const main = document.getElementById('main');
-const form = document.createElement('form');
-main.appendChild(form);
-form.id = 'evaluation-form';
-
-const select = document.createElement('select');
-const option1 = document.createElement('option');
-const option2 = document.createElement('option');
-const option3 = document.createElement('option');
-const option4 = document.createElement('option');
-select.id = 'house';
-form.appendChild(select);
-select.appendChild(option1);
-select.appendChild(option2);
-select.appendChild(option3);
-select.appendChild(option4);
-option1.id = 'gitnoria-house';
-option2.id = 'reactpuff-house';
-option3.id = 'corvinode-house';
-option4.id = 'pytherina-house';
-option1.value = 'Gitnória';
-option2.value = 'Reactpuff';
-option3.value = 'Corvinode';
-option4.value = 'Pytherina';
-option1.innerText = 'Gitnória';
-option2.innerText = 'Reactpuff';
-option3.innerText = 'Corvinode';
-option4.innerText = 'Pytherina';
-
-const getCheck = document.getElementById('checkSubject');
-form.appendChild(getCheck);
