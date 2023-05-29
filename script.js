bbd6b8d67dc45f78008705763ce80af549070d03
Thiago Pedrosa Woznika <thiagopedrosaw@gmail.com>
2023-05-29 15:54:00 -0300

    resolve 18

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 const email = document.getElementById('email');
 const password = document.getElementById('password');
-const btn = document.getElementById('submit-btn');
+const btn = document.getElementById('enter-btn');
 btn.addEventListener('click', () => {
   if (email.value === 'tryber@teste.com' && password.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
   }
+});
+
+const agreement = document.getElementById('agreement');
+const formBtn = document.getElementById('submit-btn');
+console.log(formBtn);
+
+document.addEventListener('DOMContentLoaded', () => {
+  agreement.addEventListener('change', () => {
+    console.log('checkbox alterado');
+    if (agreement.checked) {
+      console.log('o if funciona checkado');
+      formBtn.disabled = false;
+    } else {
+      console.log('o if funciona não checkado');
+      formBtn.disabled = true;
+    }
+  });
 });
