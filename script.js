81d12f2a71296722515b3252b9d69727a32e35f0
AlexandreFelippe <alemfelippe@gmail.com>
2023-05-29 11:16:28 -0300

    Resove exercício 3

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-// Ao preencher o formulário e clicar no botão, será validado que:
-// Caso o email seja "tryber@teste.com" e a senha seja "123456", será emitido um alerta contendo o texto "Olá, Tryber!";
-// Em todos os outro casos deverá ser emitido um alerta contendo o texto "Email ou senha inválidos.";
-
-// const email = document.querySelector('#input-email');
-// const password = document.querySelector('#input-password')
-// const btn = document.getElementById('submit-btn');
-// btn.addEventListener('click', () => {
-//     if (email === "tryber@teste.com" & password === "123456") {
-//         return "Olá, Tryber!"
-//     }
-//     else{
-//         return "Email ou senha inválidos."
-//     }
-// })
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const btn = document.getElementById('submit-btn');
+btn.addEventListener('click', () => {
+    if (email.value === "tryber@teste.com" && password.value === "123456") {
+       alert("Olá, Tryber!")
+    }
+    else{
+       alert("Email ou senha inválidos.")
+    }
+})
